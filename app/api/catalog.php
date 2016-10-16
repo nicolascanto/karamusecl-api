8be52ac7f02b6f7506ba518ff303a24aeb419fed
Nicolás Cantó D <nicolascanto1@gmail.com>
2016-10-15 22:13:23 -0300

    enviar karaoke

--- app/api/catalog.php
+++ app/api/catalog.php
@@ -1,12 +1,12 @@
 <?php
 
- $app->get('/api/catalog/{filter}', function($request, $response, $args){
+$app->get('/api/catalog/{filter}', function($request, $response, $args){
 
  	$filter = $args['filter'];
  	$sizePage = $request->getQueryParam('sizePage', $default = null);
  	$numPage = $request->getQueryParam('numPage', $default = null);
  	$mysqli = getConnection();
- 	$result = $mysqli->query("SELECT * FROM tbl_karaokes WHERE title LIKE '%$filter%' 
+ 	$result = $mysqli->query("SELECT id FROM tbl_karaokes WHERE title LIKE '%$filter%' 
  		AND active = true");
 
  	if ($result->num_rows > 0) {
@@ -39,4 +39,29 @@
 
  	$mysqli->close();
 
- })->add($authorization);
+})->add($authorization);
+
+$app->post('/api/catalog', function($request, $response, $args){
+
+	$title = isset($request->getParsedBody()['title']) ? $request->getParsedBody()['title'] : null;
+	$url = isset($request->getParsedBody()['url']) ? $request->getParsedBody()['url'] : null;
+
+	if (is_null($title) || is_null($url)) {
+		return $response->withJSON(array("status" => 400, "message" => "Debes especificar titulo y url"));
+	} else {
+		$mysqli = getConnection();
+		$result = $mysqli->query("INSERT INTO tbl_karaokes (title, url, active) VALUES ('$title', '$url', false)");
+		if ($result) {
+			return $response->withJSON(array("status" => 200, "message" => "Karaoke enviado", 
+				"data" => array("title" => $title, "url" => $url)));
+		} elseif ($mysqli->error_list[0]["errno"] === 1062) {
+			return $response->withJSON(array("status" => 201, "message" => "El karaoke ya fue enviado anteriormente",
+				"error" => $mysqli->error_list[0]["error"],
+				"errno" => $mysqli->error_list[0]["errno"]));
+		} else {
+			return $response->withJSON(array("status" => 400, "message" => "No se pudo enviar el karaoke"));
+		}
+	}
+
+})->add($authorization);
+
