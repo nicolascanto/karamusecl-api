835b5a88f48b83b0ea11ff750b811335216ac4cd
Nicolás Cantó D <nicolascanto1@gmail.com>
2016-11-23 21:42:48 -0300

    scrappnig

--- app/api/lib/webscraping.php
+++ app/api/lib/webscraping.php
@@ -1,92 +1,113 @@
 <?php
 
 //inicia web scraping para lista de videos de canales en Youtube
-function init($part, $chan, $test){
+// function init($part, $chan, $test){
 
-	require 'simple_html_dom.php';
+// 	require 'simple_html_dom.php';
 
-	$chan = strtolower($chan);
-	$chan = str_replace(" ", "_", $chan);
+// 	$chan = strtolower($chan);
+// 	$chan = str_replace(" ", "_", $chan);
 
-	if ($part != 0) {
-		if (!$test) {
-			echo "Insertando datos del canal " . $chan . " parte " . $part . " <br/>";
-		}
-		$url = "http://localhost/webscraping/" . $chan . "/" . $chan . "_fixed_p" . $part . ".html";
-	} else {
-		if (!$test) {
-			echo "Insertando datos del canal " . $chan . " <br/>";
-		}
-		$url = "http://localhost/webscraping/" . $chan . "/" . $chan . "_fixed.html";
-	}
+// 	if ($part != 0) {
+// 		if (!$test) {
+// 			echo "Insertando datos del canal " . $chan . " parte " . $part . " <br/>";
+// 		}
+// 		$url = "http://localhost/webscraping/" . $chan . "/" . $chan . "_fixed_p" . $part . ".html";
+// 	} else {
+// 		if (!$test) {
+// 			echo "Insertando datos del canal " . $chan . " <br/>";
+// 		}
+// 		$url = "http://localhost/webscraping/" . $chan . "/" . $chan . "_fixed.html";
+// 	}
 
 	
+// 	$html = file_get_html($url);
+// 	$arr = array();
+// 	$json = array();
+// 	$count = 0;
+
+// 	if (empty($html)) {
+
+// 		echo "el html es vacio</br> ";
+
+// 		return false;
+
+// 	} else {
+
+// 		$feed_item_container = $html->find('li[class=feed-item-container]');
+
+// 		foreach ($feed_item_container as $item) {
+// 			$feed_item_dismissable = $item->find('div[class=feed-item-dismissable]', 0);
+// 			$feed_item_main = $feed_item_dismissable->find('div[class=feed-item-main]', 0);
+// 			$feed_item_main_content = $feed_item_main->find('div[class=feed-item-main-content]', 0);
+			
+// 			$yt_lockup = $feed_item_main_content->find('div[class=yt-lockup]', 0);
+// 			$yt_lockup_dismissable = $yt_lockup->find('div[class=yt-lockup-dismissable]', 0);
+// 			$yt_lockup_content = $yt_lockup_dismissable->find('div[class=yt-lockup-content]', 0);
+// 			$yt_lockup_thumbnail = $yt_lockup_dismissable->find('div[class=yt-lockup-thumbnail]', 0);
+// 			$video_time = $yt_lockup_thumbnail->find('span[class=video-time]', 0);
+
+// 			$yt_lockup_meta =  $yt_lockup_content->find('div[class=yt-lockup-meta]', 0);
+// 			$yt_lockup_meta_info = $yt_lockup_meta->find('ul[class=yt-lockup-meta-info]', 0);
+			
+// 			$link = $yt_lockup_content->find('h3 a', 0);
+// 			$span_length = $yt_lockup_content->find('h3 span', 0);
+// 			$li_seen = $yt_lockup_meta_info->find('li', 1);
+// 			$li_published = $yt_lockup_meta_info->find('li', 0);
+// 			$span_time = $video_time->find('span', 0);
+
+// 			$url = $link->attr['href'];
+// 			$title = $link->innertext;
+// 			$length = $span_length->innertext;
+// 			$time = $span_time->innertext;
+// 			$min = substr($time, 0, -3);
+// 			$min = intval($min);
+// 			if ($min < 10) {
+// 				$time = "00:0" . $time;
+// 			} else {
+// 				$time = "00:" . $time;
+// 			}
+// 			$seen = $li_seen->innertext;
+// 			$published = $li_published->innertext;
+
+// 			$fixed_title = str_replace("'", "", $title);
+// 			$arr['title'] = $fixed_title;
+// 			$arr['url'] = 'http://youtube.com'.$url;
+// 			$arr['length'] = $length;
+// 			$arr['time'] = $time;
+// 			$pretty_seen = str_replace(" visualizaciones", "", $seen);
+// 			$pretty_seen = intval(str_replace(".", "", $pretty_seen));
+// 			$arr['seen'] = $pretty_seen;
+// 			$arr['published'] = $published;
+// 			array_push($json, $arr);
+
+// 		}
+
+// 		if ($test) {
+// 			echo json_encode($json);
+// 		} else {
+// 			return $json;
+// 		}
+// 	}
+// }
+
+function youtube_search($url_search) {
+	require_once 'simple_html_dom.php';
+
+	$url = $url_search;
 	$html = file_get_html($url);
-	$arr = array();
-	$json = array();
-	$count = 0;
 
 	if (empty($html)) {
-
-		echo "el html es vacio</br> ";
-
+		echo "EL html está vacío";
 		return false;
-
 	} else {
-
-		$feed_item_container = $html->find('li[class=feed-item-container]');
-
-		foreach ($feed_item_container as $item) {
-			$feed_item_dismissable = $item->find('div[class=feed-item-dismissable]', 0);
-			$feed_item_main = $feed_item_dismissable->find('div[class=feed-item-main]', 0);
-			$feed_item_main_content = $feed_item_main->find('div[class=feed-item-main-content]', 0);
-			
-			$yt_lockup = $feed_item_main_content->find('div[class=yt-lockup]', 0);
-			$yt_lockup_dismissable = $yt_lockup->find('div[class=yt-lockup-dismissable]', 0);
-			$yt_lockup_content = $yt_lockup_dismissable->find('div[class=yt-lockup-content]', 0);
-			$yt_lockup_thumbnail = $yt_lockup_dismissable->find('div[class=yt-lockup-thumbnail]', 0);
-			$video_time = $yt_lockup_thumbnail->find('span[class=video-time]', 0);
-
-			$yt_lockup_meta =  $yt_lockup_content->find('div[class=yt-lockup-meta]', 0);
-			$yt_lockup_meta_info = $yt_lockup_meta->find('ul[class=yt-lockup-meta-info]', 0);
-			
-			$link = $yt_lockup_content->find('h3 a', 0);
-			$span_length = $yt_lockup_content->find('h3 span', 0);
-			$li_seen = $yt_lockup_meta_info->find('li', 1);
-			$li_published = $yt_lockup_meta_info->find('li', 0);
-			$span_time = $video_time->find('span', 0);
-
-			$url = $link->attr['href'];
-			$title = $link->innertext;
-			$length = $span_length->innertext;
-			$time = $span_time->innertext;
-			$min = substr($time, 0, -3);
-			$min = intval($min);
-			if ($min < 10) {
-				$time = "00:0" . $time;
-			} else {
-				$time = "00:" . $time;
+		
+		foreach ($html->find('a') as $element) {
+			$url_base = substr($element->href, 0, 9);
+			if ($url_base == "/watch?v=") {
+				// echo $element->href . '<br>';
+				return $element->href;
 			}
-			$seen = $li_seen->innertext;
-			$published = $li_published->innertext;
-
-			$fixed_title = str_replace("'", "", $title);
-			$arr['title'] = $fixed_title;
-			$arr['url'] = 'http://youtube.com'.$url;
-			$arr['length'] = $length;
-			$arr['time'] = $time;
-			$pretty_seen = str_replace(" visualizaciones", "", $seen);
-			$pretty_seen = intval(str_replace(".", "", $pretty_seen));
-			$arr['seen'] = $pretty_seen;
-			$arr['published'] = $published;
-			array_push($json, $arr);
-
-		}
-
-		if ($test) {
-			echo json_encode($json);
-		} else {
-			return $json;
 		}
 	}
 }
--- app/api/load.php
+++ app/api/load.php
@@ -1,63 +1,133 @@
 <?php
 
-$app->get('/api/load/{channel}/{part}', function($request, $response, $args){
+// $app->get('/api/load/{channel}/{part}', function($request, $response, $args){
 
 	
-	require_once('lib/webscraping.php');
-	$part = $args['part'];
-	$channel = $args['channel'];
+// 	require_once('lib/webscraping.php');
+// 	$part = $args['part'];
+// 	$channel = $args['channel'];
 
-	$id_channel = getChannelId($channel);
+// 	$id_channel = getChannelId($channel);
 	
-	$mysqli = getConnection();
-	$json_kara = init($part, $channel, false);
+// 	$mysqli = getConnection();
+// 	$json_kara = init($part, $channel, false);
 
-	if (!$json_kara) {
-		echo "JSON is false </br>";
-	} else {
- 		//echo "item: ". $item['url'];
+// 	if (!$json_kara) {
+// 		echo "JSON is false </br>";
+// 	} else {
+//  		//echo "item: ". $item['url'];
  	
- 		// $query = "INSERT INTO tbl_karaokes (title, url, id_channel, time, seen, published, active) 
- 		// values ('$title', '$url', $id_channel, '$time', $seen, '$published', $active) 
- 		// ON DUPLICATE KEY UPDATE title='$title', time='$time', seen=$seen, published='$published', active=$active";
- 		// $result = $mysqli->query($query);
+//  		// $query = "INSERT INTO tbl_karaokes (title, url, id_channel, time, seen, published, active) 
+//  		// values ('$title', '$url', $id_channel, '$time', $seen, '$published', $active) 
+//  		// ON DUPLICATE KEY UPDATE title='$title', time='$time', seen=$seen, published='$published', active=$active";
+//  		// $result = $mysqli->query($query);
 
-	 	// if ($result) {
-	 	// 	echo "Se ha insertado el karaoke a tbl_karaokes del canal " .$channel . " <br/>";
-	 	// } else {
-	 	// 	echo "Ocurrió un error insertando, ".$mysqli->error . "<br/>";
-	 	// }
+// 	 	// if ($result) {
+// 	 	// 	echo "Se ha insertado el karaoke a tbl_karaokes del canal " .$channel . " <br/>";
+// 	 	// } else {
+// 	 	// 	echo "Ocurrió un error insertando, ".$mysqli->error . "<br/>";
+// 	 	// }
 
- 		$stmt = $mysqli->prepare("UPDATE tbl_karaokes SET time = ? WHERE url = ?");
-	 	$stmt->bind_param('ss', $time, $url);
-	 	foreach ($json_kara as $item) {
-			$title = $item['title'];
-	 		$url = $item['url'];
-	 		$time = $item['time'];
-	 		$seen = $item['seen'];
-	 		$published = $item['published'];
-	 		$active = true;
-			$stmt->execute();
-			if ($mysqli->affected_rows) {
-		 		echo "Se ha actualizado el karaoke del canal " .$channel. ", time: " .$time. "<br/>";
-		 	} else {
-		 		echo "No se actualizó el karaoke ".$mysqli->error . ", time: " .$time. "<br/>";
-		 	}
-		}
-	 	$mysqli->close();
+//  		$stmt = $mysqli->prepare("UPDATE tbl_karaokes SET time = ? WHERE url = ?");
+// 	 	$stmt->bind_param('ss', $time, $url);
+// 	 	foreach ($json_kara as $item) {
+// 			$title = $item['title'];
+// 	 		$url = $item['url'];
+// 	 		$time = $item['time'];
+// 	 		$seen = $item['seen'];
+// 	 		$published = $item['published'];
+// 	 		$active = true;
+// 			$stmt->execute();
+// 			if ($mysqli->affected_rows) {
+// 		 		echo "Se ha actualizado el karaoke del canal " .$channel. ", time: " .$time. "<br/>";
+// 		 	} else {
+// 		 		echo "No se actualizó el karaoke ".$mysqli->error . ", time: " .$time. "<br/>";
+// 		 	}
+// 		}
+// 	 	$mysqli->close();
+// 	}
+
+//  });
+
+// function getChannelId($name){
+// 	$mysqli = getConnection();
+// 	$query_channels = "SELECT id FROM tbl_channels WHERE name='" . $name . "'"; 
+// 	$resul_channels = $mysqli->query($query_channels);
+// 	$mysqli->close();
+
+// 	$row = $resul_channels->fetch_array(MYSQLI_ASSOC);
+
+// 	if ($row) {
+// 		return $row['id'];
+// 	}
+// }
+
+$app->post('/api/load/youtube/search', function($request, $response, $args){
+	$url_search = isset($request->getParsedBody()['url_search']) ? $request->getParsedBody()['url_search'] : null;
+	if (is_null($url_search)) {
+		return $response->withJSON(array("status" => 400, "message" => "url_search es null."));
 	}
+	require_once('lib/webscraping.php');
+	$url_video = youtube_search($url_search);
+	$url_video = "https://www.youtube.com" . $url_video;
+	return $response->withJSON(array("status" => 200, "url_video" => $url_video));
+	
+});
 
- });
+$app->put('/api/load/karaokes/insert/url', function($request, $response, $args){
 
-function getChannelId($name){
+	// id_karaoke es donde va a empezar el UPDATE
+	$id_karaoke = null;
+
+	$data = array();
 	$mysqli = getConnection();
-	$query_channels = "SELECT id FROM tbl_channels WHERE name='" . $name . "'"; 
-	$resul_channels = $mysqli->query($query_channels);
-	$mysqli->close();
 
-	$row = $resul_channels->fetch_array(MYSQLI_ASSOC);
-
-	if ($row) {
-		return $row['id'];
+	$result = $mysqli->query("SELECT max(id) FROM tbl_karaokes_old WHERE url IS NOT NULL ORDER BY id DESC");
+	if ($result->num_rows > 0) {
+		$row = $result->fetch_assoc();
+		$id_karaoke = $row['max(id)'] + 1;
+	} else {
+		return $response->withJSON(array("message" => "No hay resultados de max(id), id_karaoke es NULL."));
 	}
-}
+
+	$result = $mysqli->query("SELECT id, artist, song FROM tbl_karaokes_old WHERE id >= $id_karaoke");
+	if ($result->num_rows > 0) {
+		while ($row = $result->fetch_assoc()) {
+	 		$data[] = $row;
+	 	}
+
+	 	$stmt = $mysqli->prepare("UPDATE tbl_karaokes_old SET url = ? WHERE id = ?");
+ 		$stmt->bind_param('si', $url_video, $id);
+
+ 		require_once('lib/webscraping.php');
+
+	 	foreach ($data as $item) {
+	 		$id = $item['id'];
+	 		$artist = $item['artist'];
+	 		$song = $item['song'];
+	 		$full_string = $artist . '+' . $song . '+karaoke';
+	 		$search_string = str_replace(" ", "+", $full_string);
+
+	 		$url_search = "https://www.youtube.com/results?search_query=" . $search_string;
+			$url_video = youtube_search($url_search);
+			$url_video = "https://www.youtube.com" . $url_video;
+			// return $response->withJSON(array("full_string" => $full_string, "url_search" => $url_search, "url_video" => $url_video, "data" => $data));
+			$stmt->execute();
+	 	}
+
+	 	$mysqli->close();
+
+	 	/* Para revisar cuantas url son distintas a NULL
+		SELECT * FROM `tbl_karaokes_old` WHERE `url` IS NOT NULL ORDER BY `id` DESC
+	 	*/
+	}
+});
+
+
+
+
+
+
+
+
+
